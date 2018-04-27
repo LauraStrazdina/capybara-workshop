b10c495a81965cab337fbaf0ace754f2125493fb
Laura <laura.strazdina@testdevlab.com>
2018-04-27 09:14:58 +0300

    HW4

--- features/demo.feature
+++ features/demo.feature
@@ -2,3 +2,12 @@ Feature: Demo Feature
 
     Scenario: Open SignUp Page
         Given I am on Appimation home page
+        When I open Try now
+        When I enter "email", "password", "projectName" in try now view
+        Then I close Try now
+    Scenario: Open SignIn View
+        Given I am on Appimation home page
+        When I open Sign in
+        When I enter "email@gmail.com", "password" in sign up view
+        Then I try to sign in
+        Then I receive an error message
--- features/step_definitions/signup_steps.rb
+++ features/step_definitions/signup_steps.rb
@@ -1,4 +1,49 @@
 When(/^I am on Appimation home page/) do
   visit('/')
-  find(:css, '#start_button')
+  unless (find(:css, '#logo').visible?)
+    raise "logo not visible"
+  end
+end
+
+When(/^I open Try now/) do
+  find(:css, '#start_button').click
+  unless (find(:css, '#signup').visible?)
+    raise "element not visible"
+  end
+end
+
+When("I enter {string}, {string}, {string} in try now view") do |email, password, projectName|
+  find(:css, "#signup input[name='login']").send_keys(email)
+  find(:css, "#signup input[name='password1']").send_keys(password)
+  find(:css, "#signup input[name='password2']").send_keys(password)
+  find(:css, "#signup input[name='project_name']").send_keys(projectName)
+end
+
+When(/^I close Try now/) do
+  find(:css, '#signup .closecross').click
+  if (find(:css, '#signup').visible?)
+    raise "element visible"
+  end
+end
+
+When(/^I open Sign in/) do
+  find(:css, '#login-b').click
+  unless (find(:css, '#login').visible?)
+    raise "element not visible"
+  end
+end
+
+When("I enter {string}, {string} in sign up view") do |email, password|
+  find(:css, "#login input[name='login']").send_keys(email)
+  find(:css, "#login input[name='password']").send_keys(password)
+end
+
+When("I try to sign in") do
+  find(:css, '#login .button').click
+end
+
+When("I receive an error message") do
+  unless (find(:xpath, "//span[contains(text(),'Username or password is not correct')]").visible?)
+    raise "element not visible"
+  end
 end
